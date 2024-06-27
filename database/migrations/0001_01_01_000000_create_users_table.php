d3ab8669b76509a0ff32deac5788821cfd9a96ba
Océane Bondon <oceanebondon30@gmail.com>
2024-06-27 17:17:45 +0200

    commit 6

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,7 +28,6 @@ return new class extends Migration
             //$table->timestamps();
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
-
         });        
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
